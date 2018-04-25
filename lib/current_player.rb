c504a5be0d505878dd140ea70e77d37032278f80
Leticia Molina <letimolina@gmail.com>
2018-04-25 01:15:27 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,18 @@
+def turn_count(board)
+  counter = 0
+  board.each do |space|
+  if space == "X" || space == "O"
+    counter += 1
+  end
+  end
+counter
+end
+    
+ def current_player(board)
+  # board.each do |move|
+  if turn_count(board) % 2 != 0
+    "O"
+  elsif turn_count(board) % 2 == 0
+    "X"
+  end
+end
